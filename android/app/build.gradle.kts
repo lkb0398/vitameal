eef538a0f8f5a0e8cbfbd0555f18bc4862a365b4
KB <lkb0398@nate.com>
2025-12-22 11:55:15 +0900

    chore: package name change (#1) - kb
    - change_app_package_name 라이브러리 추가
    
    - 패키지명 com.example.vitamea -> com.alldayproject.vitameal

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.vitameal"
+    namespace = "com.alldayproject.vitameal"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.vitameal"
+        applicationId = "com.alldayproject.vitameal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
