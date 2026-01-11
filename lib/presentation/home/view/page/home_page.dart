6adacfe17d98ad293cfcd09edcfb311271e48383
LEE JAESOL <musou6413@naver.com>
2026-01-12 02:07:14 +0900

    fix: 탭 전환시 기존 탭이 dispose 되지 않게 수정

--- lib/presentation/home/view/page/home_page.dart
+++ lib/presentation/home/view/page/home_page.dart
@@ -8,18 +8,19 @@ import 'package:vitameal/presentation/ui_provider/home_tab_provider.dart';
 class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
+  static const _pages = [
+    MealCalendarPage(),
+    InfoPage(), // TODO : 레시피 페이지로 바꾸기
+    InfoPage(),
+  ];
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final index = ref.watch(homeTabProvider);
 
-    final pages = [
-      const MealCalendarPage(),
-      const InfoPage(), // TODO : 레시피 페이지로 바꾸기
-      const InfoPage(),
-    ];
-
     return Scaffold(
-      body: pages[index],
+      // 탭 전환시 기존탭이 dispose 되지않도록 IndexedStack 사용 (UI 상태 유지)
+      body: IndexedStack(index: index, children: _pages),
       bottomNavigationBar: CustomBottomNav(
         currentIndex: index,
         onTap: (i) {
