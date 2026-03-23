65f71d94ce1f1251ee1ff91cca9001cf3f811c4e
hailey <ahhh0325@gmail.com>
2026-03-23 18:55:45 +0900

    refactor: env 로 supabase anon key 관리

--- lib/data/data_source/eats_remote_data_source.dart
+++ lib/data/data_source/eats_remote_data_source.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:vitameal/domain/entity/eats_entity.dart';
 
@@ -11,14 +12,15 @@ class EatsRemoteDataSourceImpl implements EatsRemoteDataSource {
   // 태그별 필터링된 음식점 검색해서 불러오기
   @override
   Future<List<EatsEntity>> fetch(double lat, double lng, int? tagId) async {
+    final anonKey = dotenv.env['SUPABASE_ANON_KEY'];
+
     final response = await http.post(
       Uri.parse(
         "https://ykqdcgrimdsvuincvmtu.supabase.co/functions/v1/search-eats",
       ),
       headers: {
         "Content-Type": "application/json",
-        "Authorization":
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrcWRjZ3JpbWRzdnVpbmN2bXR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyNjY2MTgsImV4cCI6MjA4MzYyNjYxOH0.96DbUzoiQnftYE8QeFZeb71dvBIRn_8o6gnbntTTMFU",
+        if (anonKey != null) "Authorization": "Bearer $anonKey",
       },
       body: jsonEncode({"lat": lat, "lng": lng, "tagId": tagId}),
     );
